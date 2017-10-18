c5a79fc860fbbf7a05cf7f445b1b774396da400c
Brad <brad.swords@me.com>
2017-10-18 16:23:22 -0500

    Initial Commit

---
+++ GameTest1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameTest1
+//
+//  Created by Brad Swords on 10/18/17.
+//  Copyright © 2017 Brad Swords. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
